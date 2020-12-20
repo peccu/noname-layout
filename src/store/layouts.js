9a6495690719725b6b8a0b7584d245ee7b333f52
peccu <peccul@gmail.com>
2020-12-20 16:20:05 +0900

    add sample layout

--- src/store/layouts.js
+++ src/store/layouts.js
@@ -197,9 +197,33 @@ const sampleLayout = {
     }
   }
 }
+const sampleLayout2 = {
+  no: 0,
+  type: 'horizontal',
+  primary: {
+    no: 0,
+    type: 'buffer',
+    buffer: "some buffer left"
+  },
+  secondary: {
+    no: 1,
+    type: 'vertical',
+    primary: {
+      no: 1,
+      type: 'buffer',
+      buffer: "some buffer right top"
+    },
+    secondary: {
+      no: 2,
+      type: 'buffer',
+      buffer: "some buffer right bottom"
+    }
+  }
+}
 export {
   findLayoutNo,
   nearestHLayout,
   nearestVLayout,
-  sampleLayout
+  sampleLayout,
+  sampleLayout2
 }
